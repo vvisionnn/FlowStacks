eee783b422b7b0dc0cc680a83561ae4fa5795942
Ydna <0xefefef@gmail.com>
2025-05-21 00:22:10 +0800

    chore: update deps

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/vvisionnn/swiftui-presentation", from: "0.3.1"),
+    .package(url: "https://github.com/vvisionnn/swiftui-presentation", from: "0.3.2"),
   ],
   targets: [
     .target(
